dbbeb1e0cba5ed9a19905b499973699409b690fb
Release Automation <jenkins@stardog.com>
2020-05-06 16:37:57 +0000

    Release 7.3.0

--- Formula/stardog.rb
+++ Formula/stardog.rb
@@ -6,8 +6,8 @@ class Stardog < Formula
   desc "Stardog enterprise data unification platform"
   homepage "https://stardog.com"
   url "https://downloads.stardog.com/stardog/stardog-latest.zip"
-  version "7.2.1"
-  sha256 "c1727225a3f0f368d8a0549ac10f11db27bf930a96c3ef180c07c8457551a7eb"
+  version "7.3.0"
+  sha256 "62ffbc835a9a549aeb76f0f0dfd333f326d392a72040e57d021db570067fe25d"
 
   # depends_on :java => "8"
 
